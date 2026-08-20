987897e574c270bb502e7698bca61d752b46fbd4
Tonjr <wwffjr@gmail.com>
2026-08-20 11:41:30 -0300

    Adicionado meu primeiro programa em Python

---
+++ meu_primeiro_programa.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome?')
+print(f'Olá {nome} vamos começar os estudos!')
+num1 = float(input('Digite o primeiro numero:'))
+num2 = float(input('Digite o segundo numero:'))
+soma = num1 + num2
+print(f'A soma entre {num1} e {num2} é {soma:.2f}')
